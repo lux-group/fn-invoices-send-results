b36f75339f391fd707e03cdea23e4228c8c714af
ryan <ryankeeprunning@gmail.com>
2020-04-21 15:38:17 +1000

    logic completed with configuratio files ready

---
+++ src/authService/index.ts
@@ -0,0 +1,48 @@
+import LRU from "lru-cache";
+import fetch from "node-fetch";
+import querystring from "querystring";
+import { SERVICE_PASSWORD, API_HOST, APP_NAME } from "../config";
+
+class AuthServiceHTTPError extends Error {}
+
+const CACHE_TIMEOUT = 3600;
+
+const CACHE_KEY = "access-token";
+
+const cache = new LRU({ maxAge: CACHE_TIMEOUT });
+
+export async function getToken(): Promise<string> {
+  const token = cache.get(CACHE_KEY) as string;
+
+  if (token) {
+    return token;
+  }
+
+  const credentials = {
+    username: `operations+${APP_NAME}@luxgroup.com`,
+    password: SERVICE_PASSWORD,
+    grant_type: "client_credentials"
+  };
+
+  const options = {
+    method: "POST",
+    body: querystring.stringify(credentials),
+    headers: { "Content-Type": "application/x-www-form-urlencoded" }
+  };
+
+  const destination = `${API_HOST}/oauth/token`;
+
+  const response = await fetch(destination, options);
+
+  if (!response.ok) {
+    throw new AuthServiceHTTPError(
+      `${response.status} ${response.statusText} error fetching ${destination}`
+    );
+  }
+
+  const { access_token } = await response.json();
+
+  cache.set(CACHE_KEY, access_token);
+
+  return access_token;
+}
